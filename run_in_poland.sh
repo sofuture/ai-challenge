dc22b494e2f4a505282f9d7a14327752cc10ef3b
jz <jeff.zellner@gmail.com>
2011-10-27 15:21:07 -0400

    bye bye terrible languages

--- run_in_poland.sh
+++ run_in_poland.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 sh build.sh
-python tcpclient.py ants.fluxid.pl 2081 "./ocaml/_build/MyBot.native" sofuture billy999 1
+python tcpclient.py ants.fluxid.pl 2081 "./ocaml/_build/MyBot.native" sofuture billy999 10
