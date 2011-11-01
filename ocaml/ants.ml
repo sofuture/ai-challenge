a18157e1dcadd44aae0f6ea6365d35a131c47c0f
jz <jeff.zellner@gmail.com>
2011-11-01 18:38:18 -0400

    AHAHA I STILL WINNN

--- ocaml/ants.ml
+++ ocaml/ants.ml
@@ -6,27 +6,6 @@ open Unix;;
 
 open Hashtbl;;
 
-type 'a queue = ('a list * 'a list) ref;;
-
-module Queue = struct
-    exception Empty
-
-    let create () = ref ([], []);;
-
-    let add queue x =
-        let front, back = !queue in
-        queue := (x::front, back);;
-
-    let rec take queue =
-        match !queue with
-        | ([], []) ->
-            raise Empty
-        | (front, x::back) ->
-            queue := (front, back); x
-        | (front, []) ->
-            queue := ([], List.rev front);
-            take queue;;
-end;;
 
 let out_chan = open_out "mybot_err.log";;
 
--- path.ml
+++ path.ml
@@ -3,7 +3,28 @@ open Hashtbl;;
 
 open Printf;;
 
-open Queue;;
+type 'a queue = ('a list * 'a list) ref;;
+
+module Queue = struct
+    exception Empty
+
+    let create () = ref ([], []);;
+
+    let add queue x =
+        let front, back = !queue in
+        queue := (x::front, back);;
+
+    let rec take queue =
+        match !queue with
+        | ([], []) ->
+            raise Empty
+        | (front, x::back) ->
+            queue := (front, back); x
+        | (front, []) ->
+            queue := ([], List.rev front);
+            take queue;;
+end;;
+
 
 type ctype = [`Goal | `Origin | `Empty | `Blocked | `Seen];;
 type direction = [`N | `S | `E | `W];;
