24d3c485598fae05dfdce856553708a55ac6ed78
jz <jeff.zellner@gmail.com>
2011-11-02 13:16:17 -0400

    stuffs

--- make_submission.sh
+++ make_submission.sh
@@ -2,12 +2,16 @@
 
 DATE=`date +"%Y%m%d%H%M%S"`
 
+sh build.sh
+
 mkdir -p _deploy
 cp ocaml/MyBot.ml _deploy/
 cp ocaml/ants.ml _deploy/
 cp ocaml/ants.mli _deploy/
+cp ocaml/queue.ml _deploy
+cp ocaml/queue.mli _deploy
 cd _deploy
-zip MyBot.zip MyBot.ml ants.ml ants.mli
+zip MyBot.zip MyBot.ml ants.ml ants.mli queue.ml queue.mli
 mv MyBot.zip ../Bot-$DATE.zip
 cd ..
 rm -rf _deploy
